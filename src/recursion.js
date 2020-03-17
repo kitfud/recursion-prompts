1157ef822115a79312b792ba7d9e3440c3c78ad9
Kit Fuderich <Kit@chenruis-MacBook-Pro.local>
2020-03-17 16:46:05 -0700

    this is a nice comment.

--- src/recursion.js
+++ src/recursion.js
@@ -1,7 +1,7 @@
 /* jshint esversion: 6 */
 
 // Solve the following prompts using recursion.
-
+//adding in a comment.
 // 1. Calculate the factorial of a number. The factorial of a non-negative integer n,
 // denoted by n!, is the product of all positive integers less than or equal to n.
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
